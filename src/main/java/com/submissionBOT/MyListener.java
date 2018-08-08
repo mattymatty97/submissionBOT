f868665a673a7d47ca0b3b469ce31ff666ae2a7b
mattymatty <33039934+mattymatty97@users.noreply.github.com>
2018-08-08 20:42:24 +0200

    empty bot project v2

--- src/main/java/com/submissionBOT/MyListener.java
+++ src/main/java/com/submissionBOT/MyListener.java
@@ -1,5 +1,9 @@
 package com.submissionBOT;
 
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
 public class MyListener extends ListenerAdapter {
 
     @Override
